a05c5f90c1857ff419db507f3e384b34d3d8f483
a0916105 <whdofjew@gmail.com>
2021-05-02 17:12:09 +0800

    DAO

---
+++ app/src/main/java/tw/idv/jew/roomsample/UserDao.kt
@@ -0,0 +1,22 @@
+package tw.idv.jew.roomsample
+
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface UserDao {
+    @Insert
+    suspend fun insert(user: User)
+
+    @Update
+    suspend fun update(user: User)
+
+    @Delete
+    suspend fun delete(user: User)
+
+    @Query("SELECT * FROM users WHERE user = :name")
+    suspend fun getUser(name: String): User
+
+    @Query("SELECT * FROM users")
+    fun list(): Flow<List<User>>
+}
