9df5153e0d4eef0e800359d1b4c3ab36aed54185
a0916105 <whdofjew@gmail.com>
2021-05-02 17:40:21 +0800

    recyclerView Layout

--- app/src/main/java/tw/idv/jew/roomsample/MainActivity.kt
+++ app/src/main/java/tw/idv/jew/roomsample/MainActivity.kt
@@ -2,10 +2,14 @@ package tw.idv.jew.roomsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import tw.idv.jew.roomsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
